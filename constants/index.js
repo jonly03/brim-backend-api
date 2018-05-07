15f5a275397baf461b1f231aa9a03c3e5daab13d
Nelly Sugu <nelly.sugu@gmail.com>
2018-05-07 08:17:49 +0000

    Gets courts by city and latLng from Foursquare and stores courts, photos, and reviews collections on Google Cloud Firestore. Ignores .env and Firestore/serviceAccountKey.json

---
+++ constants/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    FOURSQUARE_API_BASE_URL:'https://api.foursquare.com/v2/venues',
+    FOURSQUARE_SEARCH_ROUTE:'/search',
+    FOURSQUARE_REVIEWS_ROUTE:'/tips',
+    FOURSQUARE_COURT_PHOTOS_ROUTE:'/photos',
+    FOURSQUARE_VERSION_YYYYMMDD: '20180505',
+    M_TO_MI_RATIO: 1609.344
+}
---
+++ models/Firestore/index.js
@@ -0,0 +1,10 @@
+const firestoreAdmin = require('firebase-admin');
+
+let serviceAccount = require('./serviceAccountKey.json');
+
+firestoreAdmin.initializeApp({
+  credential: firestoreAdmin.credential.cert(serviceAccount)
+});
+
+
+module.exports = firestoreAdmin.firestore();
