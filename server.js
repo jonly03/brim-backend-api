f17bfa48b4a4f1a449cfebae1a1328702c01169e
Nelly Sugu <nelly.sugu@gmail.com>
2018-05-16 09:29:36 +0000

    Enables CORS

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ let express = require('express');
 let app = express();
 let PORT = process.env.PORT || 8080;
 
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 let apiRoutes = require('./controllers/api')
 
 app.use('/api', apiRoutes);
