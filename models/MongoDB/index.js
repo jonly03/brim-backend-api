912099cab841bf4b343f3d8fb85cb7af2219aa3b
Nelly Sugu <nelly.sugu@gmail.com>
2018-07-13 22:13:03 -0700

    tightens up env check for db

--- models/MongoDB/index.js
+++ models/MongoDB/index.js
@@ -1,6 +1,6 @@
 const mongojs = require('mongojs');
 
-const mongoDB = process.env.NODE_ENV == 'production' ? (process.env.MLAB_USERNAME && process.env.MLAB_USERNAME && process.env.MLAB_PASSWORD && process.env.MLAB_DB_URL) ? mongojs(`mongodb://${process.env.MLAB_USERNAME}:${process.env.MLAB_PASSWORD}@${process.env.MLAB_DB_URL}`) : mongojs('hoopsgram') : mongojs('localhost:27017/hoopsgram');
+const mongoDB = process.env.NODE_ENV === 'production' ? (process.env.MLAB_USERNAME && process.env.MLAB_USERNAME && process.env.MLAB_PASSWORD && process.env.MLAB_DB_URL) ? mongojs(`mongodb://${process.env.MLAB_USERNAME}:${process.env.MLAB_PASSWORD}@${process.env.MLAB_DB_URL}`) : mongojs('hoopsgram') : mongojs('localhost:27017/hoopsgram');
 
 // ONLY UNCOMMENT FOR ADMIN SEEDING
 // const mongoDB = (process.env.MLAB_USERNAME && process.env.MLAB_USERNAME && process.env.MLAB_PASSWORD && process.env.MLAB_DB_URL) ? mongojs(`mongodb://${process.env.MLAB_USERNAME}:${process.env.MLAB_PASSWORD}@${process.env.MLAB_DB_URL}`) : mongojs('hoopsgram');
