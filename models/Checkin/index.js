9f87d0ed3c941ca683d2baa2a37bf35b908fc570
Nelly's Mac <nellysmac@Nellyss-MacBook-Pro.local>
2019-06-17 08:15:27 -0400

    Console.log

--- models/Checkin/index.js
+++ models/Checkin/index.js
@@ -9,14 +9,10 @@ const getCheckedInUsers = ({ court_id, requestor }) => {
 
       if (!doc || !doc[0]) return resolve([]);
 
-      let checkedInUsers = doc[0].users;
-
       // Make sure to not return the username who initiated this request if they are checked in
-      // Sometimes requestor can be undefined
-      if (!requestor) {
-        return resolve(checkedInUsers);
-      }
-      checkedInUsers = checkedInUsers.filter(
+      console.log(doc);
+      console.log(doc[0]);
+      const checkedInUsers = doc[0].users.filter(
         user => user.username !== requestor
       );
       resolve(checkedInUsers);
